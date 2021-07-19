4ebd3a0f413d157e7da4e28622c7cee53be3aef3
beejay321 <jibodu.busola@gmail.com>
2021-07-19 12:43:32 +0200

    user model

---
+++ src/models/users/schema.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  const newUser = this;
+  const plainPw = newUser.password;
+  if (newUser.isModified("password")) {
+    newUser.password = await bcrypt.hash(plainPw, 10);
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  delete userObject.__v;
+  return userObject;
+};
+
+userSchema.statics.checkCredentials = async function (email, plainPw) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const hashedPw = user.password;
+    const isMatch = await bcrypt.compare(plainPw, hashedPw);
+    if (isMatch) {
+      return user;
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
+
+export default userSchema;
